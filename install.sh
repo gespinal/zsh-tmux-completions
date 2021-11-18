d333fcf6f2715b91b075f96764070fd3ef9c7be4
gespinal <gregorioespinal@gmail.com>
2021-11-18 10:02:58 -0400

    First

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo dnf -y install vim zsh util-linux-user rsync
+chsh -s $(which zsh)
 [ -d "${HOME}/.oh-my-zsh" ] && rm -rf ${HOME}/.oh-my-zsh 2> /dev/null
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -6,3 +8,4 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 rsync -avz ${HOME}/.zsh-tmux-completions/ ${HOME}/ --exclude .git --exclude install.sh
 exec zsh
+
