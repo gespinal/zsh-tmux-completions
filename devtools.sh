7e09033d4abe896569c648ebb2b856ca897f420f
Gregorio Espinal <gregorioespinal@gmail.com>
2022-12-29 18:14:23 -0400

    Fix for docker server

--- devtools.sh
+++ devtools.sh
@@ -9,6 +9,8 @@ sudo apt upgrade -y
 # sudo apt install code 
 
 # Docker
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg2 pass -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
